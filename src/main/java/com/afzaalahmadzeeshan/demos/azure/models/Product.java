3f9cb61ea589ff761780f1e429bb44f4fb55599a
Afzaal Ahmad Zeeshan <afzaal.ahmad.zeeshan@hotmail.com>
2022-03-22 22:07:27 +0100

    the first commit, more to come...

---
+++ src/main/java/com/afzaalahmadzeeshan/demos/azure/models/Product.java
@@ -0,0 +1,49 @@
+package com.afzaalahmadzeeshan.demos.azure.models;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private boolean available;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
